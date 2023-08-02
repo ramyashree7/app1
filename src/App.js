acabcedd1ac9615b932e222149c61b4bacb3d0cc
Ramyashree <ramyaanchan2000@example.com>
2023-08-02 14:23:18 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
+import EmployeeDataTable from "./components/EmployeeDataTable";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container card mb-4 box-shadow">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">
+        React CRUD Example
+        </h4>
+      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/read" />} />
+        <Route exact path="/create" element={<AddEmployee />} />
+        <Route exact path="/read" element={<EmployeeDataTable />} />
+        <Route path="/edit/:id" element={<EditEmployee />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
