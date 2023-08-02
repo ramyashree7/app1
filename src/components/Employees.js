b6ad4cbfde54cd0436613b9b51813c2bb0042569
Ramyashree <ramyaanchan2000@example.com>
2023-08-02 14:23:04 +0530

    commit10

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees =[
+    {
+        id:"1",
+        Name:"Manoj",
+        Age:"30"
+    },
+    {
+        id:"2",
+        Name:"Ram",
+        Age:"20"
+    },
+    {
+        id:"3",
+        Name:"Raj",
+        Age:"31"
+    },
+]
+export default Employees;
