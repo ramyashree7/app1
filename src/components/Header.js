474f96a6041a16c1cda57de328f188f809b4e7c8
Ramyashree <ramyaanchan2000@example.com>
2023-08-05 10:57:37 +0530

    fixing 4

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+      <h1 className="header"> Sign Up</h1>
+    </div>
+  );
+}
+
+export default Header;
