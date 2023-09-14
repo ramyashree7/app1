f8f16f67cc016f5a8d06d564971fa00286f13db0
ramyashree7 <ramyaanchan2000@gmail.com>
2023-09-14 11:41:48 +0530

    changes in nav1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/styles/main.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import News from "./pages/News";
+import Performance from "./pages/Performance";
+import Transactions from "./pages/Transactions";
+import Settings from "./pages/Settings";
+import Support from "./pages/Support";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Sidebar />
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/news" element={<News />} />
+                    <Route path="/performance" element={<Performance />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/support" element={<Support />} />
+                    <Route path="/transactions" element={<Transactions />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
